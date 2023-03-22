18d2e429622b57e5443f493083605161e60fb85f
CindyCingo <cindycingo@gmail.com>
2023-03-22 08:02:37 -0700

    task -9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * times_table - prints the 0 times table,starting with 0
+ * times_table - prints the 0 times table, starting with 0
  * Return: empty output
  */
 
@@ -17,7 +17,7 @@ void times_table(void)
 	z = x * y;
 	if (z > 9)
 	{
-	u = z & 10;
+	u = z % 10;
 	d = (z - u) / 10;
 	_putchar(44);
 	_putchar(32);
