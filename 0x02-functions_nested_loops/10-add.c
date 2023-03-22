dd076a91a1a89bec7e20b3624775bcace1fd4fe1
CindyCingo <cindycingo@gmail.com>
2023-03-22 07:46:51 -0700

    task - 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,24 +1,18 @@
 #include "main.h"
 
+
 /**
- * print_alphabet_x10 - make alphabet x10 times
- * Return: void
+ * add - Adds two values
+ * @n1: The first number to be computed
+ * @n2: The second number to be computed
+ * Return: Integer value
  */
 
-void print_alphabet_x10(void)
+int add(int n1, int n2)
 
 {
 
-	char c;
-	int i = 0;
+	return (n1 + n2);
 
-	while (i <= 9)
-	{
-	for (c = 'a'; c <= 'z'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
-	i++;
-	}
 }
+
