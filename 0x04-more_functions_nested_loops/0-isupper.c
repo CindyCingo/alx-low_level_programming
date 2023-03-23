561bf228c698840b0c3b49b5b946cee7736cda5a
CindyCingo <cindycingo@gmail.com>
2023-03-23 15:32:09 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 
 /**
  * _isupper - check if c is upper
-
+ *
  * @c: input for alphabet
  *
  * Return: 1 if its uppercase, 0 if not
@@ -10,7 +10,7 @@
 
 int _isupper(int c)
 {
-	if (c >= 64 && c <= 90)
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
